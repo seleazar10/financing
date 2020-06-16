c616c3248fa0da63f19c944263231a76e602c1fa
Stama Eleazar <eleoatam10@gmail.com>
2020-06-16 12:46:37 -0500

    controller set up to retreive input

--- scipt.js
+++ scipt.js
@@ -17,6 +17,33 @@ var UIController = (function () {
 
     //some code
 
+
+    return {
+
+        getInput: function () {
+            console.log("here's the input")
+
+            return {
+
+
+                //value of type
+                inputType: document.querySelector('.add_type').value,
+                //value of descptiption
+                inputDescription: document.querySelector('.add_description').value,
+                //value of amount
+                inputValue: document.querySelector('.add_value').value,
+
+
+            }
+
+
+
+        }
+
+
+    }
+
+
 })();
 
 
@@ -26,7 +53,6 @@ var UIController = (function () {
 
 var controller = (function (budgetCtrl, UICtrl) {
 
-    //some code
 
     //button event listener
 
@@ -43,9 +69,11 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         if (event.keyCode === 13 || event.which === 13) {
             console.log('Enter is pressed')
+            event.preventDefault()
             ctrlAddItem()
+
         } else {
-            console.log(`key is not Enter... this is the key ${event.keyCode}`)
+            console.log(`this is not the "ENTER" key... this is key: ${event.keyCode}`)
         }
 
 
@@ -54,10 +82,15 @@ var controller = (function (budgetCtrl, UICtrl) {
 
     //function to execute once btn or enter key is pressed!
 
-   var ctrlAddItem = function() {
-        console.log('btn is clicked, function will be exeuted')
+    var ctrlAddItem = function () {
+        console.log('function will be executed')
 
         //1. get value input
+
+        var inputInfo = UIController.getInput()
+        console.log(inputInfo)
+
+
         //2. add items to the budget controller
         //3. Add item to UI
         //4. Calcualte the budget
